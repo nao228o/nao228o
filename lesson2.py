4c37c9c9a3ec713a4b6afd4b1c9aa7eef6dd3b08
nao228o <97509908+nao228o@users.noreply.github.com>
2022-02-14 10:46:56 +0900

    lesson2.py
    only page in shop infomation for scraping

---
+++ lesson2.py
@@ -0,0 +1,41 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+chrome_path = '/Users/tsushima/Desktop/ScrapingBeginner-main/chromedriver 3'
+
+options = Options()
+options.add_argument('--incognito')
+
+driver = webdriver.Chrome(executable_path=chrome_path, options=options)
+driver.get('https://hitosara.com/0006047210/')
+
+sleep(3)
+
+shop_info = driver.find_element(By.CLASS_NAME, 'shop_info')
+elems_th = shop_info.find_elements(By.TAG_NAME, 'th')
+keys = []
+for elem_th in elems_th:
+    key = elem_th.text
+    keys.append(key)
+print(keys)
+
+elems_td = shop_info.find_elements(By.TAG_NAME, 'td')
+values = []
+for elem_td in elems_td:
+    value = elem_td.text
+    values.append(value)
+print(values)
+
+sleep(2)
+
+df = pd.DataFrame()
+df['項目'] = keys
+df['値'] = values
+
+df.to_csv("ヒトサラ.csv", index=False)
+
+driver.quit()
