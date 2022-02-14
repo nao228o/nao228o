3ab83dc2c9dea4f2873e37e35d0a224be9fcdf0f
nao228o <97509908+nao228o@users.noreply.github.com>
2022-02-14 10:58:17 +0900

    hitosara update0214
    included lesson2.py

--- lesson.py
+++ lesson.py
@@ -2,8 +2,10 @@ from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import pandas as pd
 
-chrome_path = "/Users/tsushima/Desktop/ScrapingBeginner-main/chromedriver 3"
+chrome_path = '/Users/tsushima/Desktop/ScrapingBeginner-main/chromedriver 3'
 
 options = Options()
 options.add_argument('--incognito')
@@ -13,36 +15,38 @@ driver.get('https://hitosara.com/aichi/lst/')
 
 sleep(3)
 
-element = driver.find_element_by_class_name('shop-info-wrapper')
-aTag    = element.find_element_by_tag_name("a")
-url     = aTag.get_attribute("href")
-print("test1 OK!!")
+element = driver.find_element(By.CLASS_NAME, 'shop-info-wrapper')
+aTag    = element.find_element(By.TAG_NAME, 'a')
+url     = aTag.get_attribute('href')
+print('test1 OK!!')
 
 driver2 = driver.get(url)
 
 sleep(3)
 
-elems_th = driver2.find_elements_gy_tag_name('th')
+shop_info = driver2.find_element(By.CLASS_NAME, 'shop_info')
+elems_th = shop_info.find_elements(By.TAG_NAME, 'th')
 keys = []
-for i,elem_th in elems_th:
-    if elem_th[i] == None:
-        elem_th[i] == elem_th[i]
-    else :
-        key = elem_th.text
-    keys.append[key]
-
+for elem_th in elems_th:
+    key = elem_th.text
+    keys.append(key)
 print(keys)
 
-elems_td = driver2.find_elements_gy_tag_name('td')
+elems_td = shop_info.find_elements(By.TAG_NAME, 'td')
 values = []
-for i,elem_td in elems_td:
-    if elem_td[i] == None:
-        elem_td[i] == elem_td[i]
-    else:
-        value = elem_td.text
-    values.append[value]
-
+for elem_td in elems_td:
+    value = elem_td.text
+    values.append(value)
 print(values)
+
+sleep(2)
+
+df = pd.DataFrame()
+df['項目'] = keys
+df['値'] = values
+
+df.to_csv("ヒトサラ.csv", index=False)
+
 driver.back()
 
 sleep(2)
